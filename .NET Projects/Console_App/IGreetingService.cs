47c8fbf08cf511b6a343c3c8058ca24ffccc5919
BilalAbdulkany <bilal.abdulkany@gmail.com>
2021-08-31 17:24:45 +0400

    '.NET'

---
+++ .NET Projects/Console_App/IGreetingService.cs
@@ -0,0 +1,8 @@
+//DI, logging, settings
+namespace Console_App
+{
+    public interface IGreetingService
+    {
+        void Run();
+    }
+}
---
+++ .NET Projects/EisCoreNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EisCoreNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
